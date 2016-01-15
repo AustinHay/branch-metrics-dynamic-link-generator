683f74aa72ea091bfc83a4219097027485762e5e
Austin Hay <hayaustin@gmail.com>
2016-01-14 18:38:19 -0800

    Added encoding

--- link-generator.js
+++ link-generator.js
@@ -1,4 +1,6 @@
 
+// Dynamic Link Generator Function
+
 function createDynamicLink() {
 	// Takes inputs and creates a link out of them.
 
@@ -99,5 +101,115 @@ function createDynamicLink() {
 		var link = link + '&' + 'tags[]=' + tag5;
 	}
 
-window.document.getElementById('generatedDyanmicLink').innerHTML = link
-}
+	// // Add Branch Reserved Values
+	iosUrl = window.document.getElementById('iosUrl').value;
+	androidUrl = window.document.getElementById('androidUrl').value;
+	desktopUrl = window.document.getElementById('desktopUrl').value;
+	windowsUrl = window.document.getElementById('windowsUrl').value;
+	blackberryUrl = window.document.getElementById('blackberryUrl').value;
+	fireUrl = window.document.getElementById('fireUrl').value;
+	fallbackUrl = window.document.getElementById('fallbackUrl').value;
+	afterClickUrl = window.document.getElementById('afterClickUrl').value;
+	deeplink_path = window.document.getElementById('deeplink_path').value; 
+	ogTitle = window.document.getElementById('ogTitle').value;
+	ogDescription = window.document.getElementById('ogDescription').value;
+	ogImage = window.document.getElementById('ogImage').value;
+	ogVideo = window.document.getElementById('ogVideo').value;
+	ogUrl = window.document.getElementById('ogUrl').value;
+	ogType = window.document.getElementById('ogType').value;
+	ogRedirect = window.document.getElementById('ogRedirect').value;
+    
+    var branchReservedData = {};
+
+   		if (iosUrl.length > 0) {
+    		branchReservedData['$ios_url']=iosUrl;
+    	}
+
+    	 if (androidUrl.length > 0) {
+    		branchReservedData['$android_url']=androidUrl;
+		}
+
+		if (desktopUrl.length > 0) {
+    		branchReservedData['$deskto_url']=desktopUrl;
+		}
+
+	// 	if (windowsUrl.length > 0) {
+	// 		'$windows_url'= windowsUrl;
+	// 	}
+
+	// 	if (blackberryUrl.length > 0) {
+	// 		'$blackberry_url'= blackberryUrl;
+	// 	}
+
+	// 	if (fireUrl.length > 0) {
+	// 		'$fire_url'= fireUrl;
+	// 	}
+
+	// 	if (fallbackUrl.length > 0) {
+	// 	'$fallback_url'= fallbackUrl;
+	// 	}	
+
+	// 	if (afterClickUrl.length > 0) {
+	// 		'$after_click_url'=afterClickUrl;
+	// 	}
+
+	// 	if (deeplink_path.length > 0) {
+	// 		'$deeplink_path'=deeplink_path;
+	// 	}
+
+	// 	if (ogTitle.length > 0) {
+	// 		'$og-title'=ogTitle;
+	// 	}
+
+	// 	if (ogDescription.length > 0) {
+	// 		'$og-description'=ogDescription;
+	// 	}
+
+	// 	if (ogImage.length > 0) {
+	// 		'$og-image'=ogImage;
+	// 	}
+
+	// 	if (ogVideo.length > 0) {
+	// 		'$og-video'=ogVideo;
+	// 	}
+
+	// 	if (ogUrl.length > 0) {
+	// 		'$og-url'=ogUrl;
+	// 	}
+
+	// 	if (ogType.length > 0) {
+	// 		'$og-type'=ogType;
+	// 	}
+
+	// 	if (ogRedirect.length > 0) {
+	// 		'$og-redirect'=ogRedirect;
+	//     }
+
+	// JSON Convertion
+	JSON.stringify(branchReservedData);	
+    console.log(branchReservedData);
+
+	// Base 64 Encoding
+    if (jQuery.isEmptyObject(branchReservedData) != true ) {
+		var base64data = window.btoa(JSON.stringify(branchReservedData));
+		window.document.getElementById('encodedData').value = base64data;
+
+		// Form new link with data added in.
+		link = link + 'data=' + base64data + '==';
+	}
+		
+	// Base64 Encoded Data Validation
+	var unencodedData = window.atob(base64data);
+	console.log(unencodedData)
+
+	window.document.getElementById('unencodedData').value = unencodedData;
+
+	// Final Link Creation
+	window.document.getElementById('generatedDyanmicLink').value = link
+};
+
+// Clipboard Function
+function copyToClipboard(text) {
+	document.getElementById('generatedDyanmicLink').innerHTML
+  	window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+};
